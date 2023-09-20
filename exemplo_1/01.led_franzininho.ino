ca8a92cd020e9b9c5be2dad2e099b44a7d4137b2
Diana Santos <dianaipsantos@hotmail.com>
2023-09-19 21:52:56 -0300

    Inserindo cód led

---
+++ exemplo_1/01.led_franzininho.ino
@@ -0,0 +1,21 @@
+/*
+    Autor(a): Diana Santos - Franzininho
+    Data: 08/08/23
+    Título: Exemplo 1 - Desvendando a Luz: Criando um Sinalizador com LED
+    Descrição: Neste exemplo vamos  criar uma luz de sinalização, 
+    para isso vamos acionar o Led 21 da placa Franzininho Wifi,
+    fazendo-o piscar em intervalos regulares 1 segundo.
+*/
+
+const int led = 13; // Pino digital onde o LED está conectado
+
+void setup() {
+  pinMode(led, OUTPUT); // Configura o pino do LED como saída
+}
+
+void loop() {
+  digitalWrite(led, HIGH); // Liga o LED
+  delay(1000);                // Mantém ligado por 1 segundo
+  digitalWrite(led, LOW);  // Desliga o LED
+  delay(1000);                // Mantém desligado por 1 segundo
+}
